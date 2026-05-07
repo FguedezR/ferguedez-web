80a4e2c8ba0735109b8df1c05c286923e43259b4
Fernando <fernanguedezr@gmail.com>
2026-05-07 19:03:58 +0200

    feat: add cloudflare adapter and initial styles/scripts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://ferguedez.dev",
+  adapter: cloudflare()
+});
